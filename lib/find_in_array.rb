6e00d8282d513e3ffec722bcdbd7eccabb3097a4
Jason Herrera <baytownjason@yahoo.com>
2020-12-14 01:55:30 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,5 +1,6 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
+  array.index(value_to_find)
  # i = 0
   #append = []
   #for i in array do
@@ -7,10 +8,10 @@ def find_element_index(array, value_to_find)
   #    return i
   #  else
   #    append.push(0)
-    end
-    i += 1
-  end
-  if append.length == array.length
-    return nil
-  end
+   # end
+   # i += 1
+ # end
+ # if append.length == array.length
+ #   return nil
+ # end
 end
